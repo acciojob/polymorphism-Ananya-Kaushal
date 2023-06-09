f999396c15a1a634f61718678c6ef0c2b4fb17bc
Ananya-Kaushal <ananyakaushal1825@gmail.com>
2023-06-09 20:39:10 +0530

    overloading example

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,16 +9,16 @@ public class Main {
     }
     public static class Product
     {
-        public int Product(int x,int y)
+        public int product(int x,int y)
         {
             return x*y;
         }
         //Method Overlaoding
-        public int Product(int x,int y,int z)
+        public int product(int x,int y,int z)
         {
             return x*y*z;
         }
-        public  double Product(double x,double y)
+        public  double product(double x,double y)
         {
             return x*y;
         }
