924feba6ea2f7b0d12801c423ff56c3fb07f7a0d
Ananya-Kaushal <ananyakaushal1825@gmail.com>
2023-06-09 20:24:00 +0530

    Overloading is done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p=new Product();
+        System.out.println(p.Product(4,5));
+        System.out.println(p.Product(4,5,6));
+        System.out.println(p.Product(4.25,7.34));
+    }
+    static class Product
+    {
+        public int Product(int x,int y)
+        {
+            return x*y;
+        }
+        //Method Overlaoding
+        public int Product(int x,int y,int z)
+        {
+            return x*y*z;
+        }
+        public  double Product(double x,double y)
+        {
+            return x*y;
+        }
+    }
 
 }
